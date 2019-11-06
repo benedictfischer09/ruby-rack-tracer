2a1538eedd47b55377e45e718bf200727f7ef0de
Ben Fischer <ben.fischer.810@gmail.com>
2019-11-06 11:28:55 -0600

    try with block format

--- lib/rack/tracer.rb
+++ lib/rack/tracer.rb
@@ -18,15 +18,9 @@ module Rack
     #        as errors. Errors are **not** muted by the middleware, they're re-raised afterwards.
     def initialize(app, # rubocop:disable Metrics/ParameterLists
                    tracer: OpenTracing.global_tracer,
-                   on_start_span: nil,
-                   on_finish_span: nil,
-                   trust_incoming_span: true,
                    errors: [StandardError])
       @app = app
       @tracer = tracer
-      @on_start_span = on_start_span
-      @on_finish_span = on_finish_span
-      @trust_incoming_span = trust_incoming_span
       @errors = errors
     end
 
@@ -40,8 +34,10 @@ module Rack
 
       method = env[REQUEST_METHOD]
 
-      context = @tracer.extract(OpenTracing::FORMAT_RACK, env) if @trust_incoming_span
-      scope = @tracer.start_active_span(
+      context = @tracer.extract(OpenTracing::FORMAT_RACK, env)
+      result = nil
+
+      @tracer.start_active_span(
         method,
         child_of: context,
         tags: {
@@ -50,35 +46,29 @@ module Rack
           'http.method' => method,
           'http.url' => env[REQUEST_URI]
         }
-      )
-      span = scope.span
+      ) do |scope|
+        span = scope.span
+        env['rack.span'] = span
 
-      @on_start_span.call(span) if @on_start_span
+        result = @app.call(env).tap do |status_code, _headers, _body|
+          span.set_tag('http.status_code', status_code)
 
-      env['rack.span'] = span
-
-      @app.call(env).tap do |status_code, _headers, _body|
-        span.set_tag('http.status_code', status_code)
-
-        route = route_from_env(env)
-        span.operation_name = route if route
+          route = route_from_env(env)
+          span.operation_name = route if route
+        end
+      rescue *@errors => e
+        span.set_tag('error', true)
+        span.log_kv(
+          event: 'error',
+          :'error.kind' => e.class.to_s,
+          :'error.object' => e,
+          message: e.message,
+          stack: e.backtrace.join("\n")
+        )
+        raise
       end
-    rescue *@errors => e
-      span.set_tag('error', true)
-      span.log_kv(
-        event: 'error',
-        :'error.kind' => e.class.to_s,
-        :'error.object' => e,
-        message: e.message,
-        stack: e.backtrace.join("\n")
-      )
-      raise
-    ensure
-      begin
-        scope.close
-      ensure
-        @on_finish_span.call(span) if @on_finish_span
-      end
+
+      result
     end
 
     private
